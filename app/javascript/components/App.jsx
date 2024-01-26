ccea0748ab5ed1bd2c9a3f5a41ae5fdadba16d1e
mangatgrinpal <mangatgrinpal@gmail.com>
2024-01-25 18:21:48 -0800

    added react-router and home component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { 
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+	Outlet,
+	Route,
+	Link
+} from 'react-router-dom';
+import Home from './Home';
+// import Dashboard from './Dashboard';
 
 function App() {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<Root />}>
+				<Route index element={<Home />}/>
+				{/* <Route element={<Dashboard />}/> */}
+			</Route>
+		)
+	)
 	return (
-		<div>App</div>
+		<div>
+			<RouterProvider router={router}/>
+		</div>
+	)
+}
+
+const Root = () => {
+	return (
+		<>
+			<div>
+				<Link to="/">Home</Link>
+				{/* <Link to="/dashboard">Dashboard</Link> */}
+			</div>
+			<div>
+				<Outlet />
+			</div>
+		</>
 	)
 }
 
